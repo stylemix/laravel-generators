65d9f7264502083549d1fd382fa01062b638f078
Azamat <azamat@stylemix.net>
2019-04-15 17:58:23 +0500

    Field class names updated.

--- resources/stubs/form_resource.blade.php
+++ resources/stubs/form_resource.blade.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Forms;
 
-use Stylemix\Base\Fields\Email;
-use Stylemix\Base\Fields\Input;
-use Stylemix\Base\Fields\Textarea;
+use Stylemix\Base\Fields\CheckboxField;
+use Stylemix\Base\Fields\CheckboxesField;
+use Stylemix\Base\Fields\EmailField;
+use Stylemix\Base\Fields\NumberField;
+use Stylemix\Base\Fields\PasswordField;
+use Stylemix\Base\Fields\SelectField;
+use Stylemix\Base\Fields\TextField;
+use Stylemix\Base\Fields\TextareaField;
 use Stylemix\Base\FormResource;
 
 class {{ $class }} extends FormResource
